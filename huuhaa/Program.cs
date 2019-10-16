96e57e45626bd15f00c0e62a4b0a8361fba90c35
Leo Vainio <N4527@student.jamk.fi>
2019-10-16 17:03:01 +0300

    Add project files.

---
+++ huuhaa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace huuhaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Haist itte!");
+        }
+    }
+}
